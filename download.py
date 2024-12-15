526fc454bf9ba7d3654c825d583da42922285af0
Antoine Gleisberg <antoine.gleisberg@gmail.com>
2024-12-15 11:17:09 +0100

    updating to allow direct url download

--- download.py
+++ download.py
@@ -2,6 +2,7 @@ from os import path
 from typing import List, Mapping
 
 from pytube import YouTube
+
 # import youtube_dl
 import yt_dlp as youtube_dl
 from moviepy.editor import VideoFileClip
@@ -13,12 +14,13 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-class YoutubeScraper():
+
+class YoutubeScraper:
     @staticmethod
     def search(*keywords: str, wait_time: int = 2) -> str:
         driver = webdriver.Chrome()
         query: str = "https://www.youtube.com/results?search_query=" + "+".join(keywords)
-        
+
         try:
             driver.get(query)
             driver.implicitly_wait(wait_time)
@@ -42,10 +44,11 @@ class YoutubeScraper():
         }
         return video_info
 
-class HTMLParser():
+
+class HTMLParser:
     @staticmethod
     def find_ytb_videos(page_source: str, limit: int = 10) -> List[str]:
-        soup = BeautifulSoup(page_source, 'html.parser')
+        soup = BeautifulSoup(page_source, "html.parser")
         contents = soup.find(id="contents").find("ytd-item-section-renderer").find(id="contents")
         results = []
 
@@ -61,16 +64,22 @@ class HTMLParser():
         return results
 
 
-class YoutubeDownloader():
+class YoutubeDownloader:
     @staticmethod
     def download(url: str, folder: str) -> bool:
         methods = [YoutubeDownloader._download_pytube, YoutubeDownloader._download_ytdl]
-        
+
         success = False
         i = 0
         while not success and i < len(methods):
             success = methods[i](url, folder)
-        
+            i += 1
+
+        if not success:
+            print("Download failed\n")
+        else:
+            print("Download successful\n")
+
         return success
 
     @staticmethod
@@ -82,6 +91,7 @@ class YoutubeDownloader():
             video_path = yt.streams.filter(file_extension="mp4").get_highest_resolution().download(folder)
             return True
         except:
+            print("Download using pytube failed")
             return False
 
     @staticmethod
@@ -95,10 +105,11 @@ class YoutubeDownloader():
                 ydl.download([url])
             return True
         except:
+            print("Download using youtube_dl failed")
             return False
 
 
-class AudioConverter():
+class AudioConverter:
     @staticmethod
     def convert_videos_to_audio(video_folder: str, audio_folder: str) -> None:
         for video_path in Path(video_folder).iterdir():
@@ -107,10 +118,25 @@ class AudioConverter():
             video.audio.write_audiofile(f"{audio_folder}/{name}")
 
 
-class DownloadManager():
-    def __init__(self, default_number_videos: int = 3, target_audio_folder: str = "mp3s", target_video_folder: str = "videos", tmp_folder: str = "tmp") -> None:
-        self.save_audio: bool = self._get_input("Do you want to extract the audio from downloaded videos ? yes / no : ", ["yes", "no"]) == "yes"
+class DownloadManager:
+    def __init__(
+        self,
+        default_number_videos: int = 3,
+        target_audio_folder: str = "mp3s",
+        target_video_folder: str = "videos",
+        tmp_folder: str = "tmp",
+    ) -> None:
+        self.save_audio: bool = (
+            self._get_input("Do you want to extract the audio from downloaded videos ? yes / no : ", ["yes", "no"])
+            == "yes"
+        )
         print("not " * (not self.save_audio) + "saving audio\n")
+
+        self.search_urls: bool = (
+            self._get_input("Do you want to search for videos or input URLs ? search / url : ", ["search", "url"])
+            == "url"
+            )
+        print()
 
         self.n_videos = default_number_videos
         self.audio_folder = target_audio_folder
@@ -119,18 +145,39 @@ class DownloadManager():
 
     def run(self):
         while True:
-            answer = input("Enter a query (i.e. list of keywords) to search on youtube. Type \"quit\" to exit program.\n")
-            if answer.lower() == "quit":
-                break
+            if self.search_urls:
+                answer = input('Enter a youtube URL to download. Type "quit" to exit program. Type "search" to switch to search mode\n')
+                if answer.lower() == "quit":
+                    break
 
-            self._handle_query(*answer.split(" "))
-        
+                if answer.lower() == "search":
+                    self.search_urls = False
+                    continue
+
+                self._download_via_url(answer)
+            else:
+                answer = input('Enter a query (i.e. list of keywords) to search on youtube. Type "quit" to exit program. Type "url" to switch to URL mode\n')
+                if answer.lower() == "quit":
+                    break
+
+                if answer.lower() == "url":
+                    self.search_urls = True
+                    continue
+
+                self._handle_query(*answer.split(" "))
+
         if self.save_audio:
             AudioConverter.convert_videos_to_audio(self.tmp_folder, self.audio_folder)
 
+    def _download_via_url(self, url: str):
+        info = YoutubeScraper.get_video_info(url)
+        print(f"""\nDownloading {info["Title"]}, from {info["Author"]}...""")
+        YoutubeDownloader.download(url, self.tmp_folder)
+
+
     def _handle_query(self, *keywords, query_wait_time: int = 2):
         page_content = YoutubeScraper.search(*keywords, wait_time=query_wait_time)
-        
+
         n_videos = self.n_videos
         while True:
             video_links = HTMLParser.find_ytb_videos(page_content, n_videos)
@@ -143,10 +190,21 @@ class DownloadManager():
                 print("------------------")
                 print()
 
-            if len(video_links) < n_videos:
-                answer = self._get_input("Type in the number of the video you want to download, or type 'cancel' to cancel. Type 'restart' to search for videos again with more loading time for the browser. \n", ["cancel", "restart"] + [f"{i+1}" for i in range(n_videos)])
+            if len(video_links) == 0:
+                answer = self._get_input(
+                    "No videos were found. Type 'cancel' to cancel. Type 'restart' to search for videos again with more loading time for the browser. Type 'url' to manually enter a URL to download\n",
+                    ["cancel", "restart", "url"] + [f"{i+1}" for i in range(n_videos)],
+                )
+            elif len(video_links) < n_videos:
+                answer = self._get_input(
+                    "Type in the number of the video you want to download, or type 'cancel' to cancel. Type 'restart' to search for videos again with more loading time for the browser. \n",
+                    ["cancel", "restart"] + [f"{i+1}" for i in range(n_videos)],
+                )
             else:
-                answer = self._get_input("Type in the number of the video you want to download, type 'more' to see more options, or type 'cancel' to cancel. \n", ["more", "cancel"] + [f"{i+1}" for i in range(n_videos)])
+                answer = self._get_input(
+                    "Type in the number of the video you want to download, type 'more' to see more options, or type 'cancel' to cancel. \n",
+                    ["more", "cancel"] + [f"{i+1}" for i in range(n_videos)],
+                )
 
             if answer == "more":
                 n_videos *= 2
@@ -158,31 +216,32 @@ class DownloadManager():
                     t = input("How much time do you want to give the browser to look for videos ? \n")
                 self._handle_query(*keywords, t)
                 return
-
+            elif answer == "url":
+                self._download_via_url()
+                return
             else:
                 chosen_video_idx = int(answer) - 1
                 break
-        
+
         url = video_links[chosen_video_idx]
         info = YoutubeScraper.get_video_info(url)
         print(f"""\nDownloading {info["Title"]}, from {info["Author"]}...\n""")
         YoutubeDownloader.download(video_links[chosen_video_idx], self.tmp_folder)
-
 
     def _show_video_info(self, url):
         info = YoutubeScraper.get_video_info(url)
         for key, value in info.items():
             print(f"{key}: {value}")
 
-    
     def _get_input(self, message: str, accepted_values: List[str]) -> str:
         result = None
         while result not in accepted_values:
             result = input(message)
         return result
 
+
 if __name__ == "__main__":
-    
+
     download_manager = DownloadManager()
     download_manager.run()
 
